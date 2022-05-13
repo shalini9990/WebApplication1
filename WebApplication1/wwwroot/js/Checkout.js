92ee407671cda56462c3e7c7f8865ba899699eeb
shalini9990 <bcs21030003@student.uts.edu.my>
2022-05-13 16:14:16 +0800

    Add project files.

---
+++ WebApplication1/wwwroot/js/Checkout.js
@@ -0,0 +1,30 @@
+searchForm = document.querySelector('.search-form');
+ 
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    navbar.classList.remove('active');
+}
+ 
+let shoppingCart = document.querySelector('.shopping-cart');
+ 
+document.querySelector('#cart-btn').onclick = () =>{
+    shoppingCart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+ 
+let navbar = document.querySelector('.navbar');
+ 
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+}
+ 
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    navbar.classList.remove('active');
+}
+ 
